900db8f5148ca55d862d085decac4d4a7a66edbf
huzai786 <muhammadhuzaifagamer123@gmail.com>
2022-11-13 11:34:20 -0800

    version 1.0

---
+++ main.pyw
@@ -0,0 +1,20 @@
+import time
+
+import schedule
+
+from whatsapp.whatsapp import Person, Job, Schedular
+
+
+hafsa = Person(number='************')
+hafsa_job1 = Job(time='10:55', reminder='checking')
+hafsa_job2 = Job(time='11:00', reminder='checking2')
+hafsa_job3 = Job(time='11:33', reminder='checking3')
+
+hafsa_reminder = Schedular(hafsa, hafsa_job1, hafsa_job2, hafsa_job3)
+hafsa_reminder.schedule_reminder()
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
---
+++ whatsapp/whatsapp.py
@@ -0,0 +1,69 @@
+from typing import NamedTuple
+
+import pyautogui as pg
+import schedule
+
+
+class Job(NamedTuple):
+    time: str
+    reminder: str
+
+
+class Person:
+    def __init__(self, number):
+        assert type(number) is str
+        self.number = str(number)
+
+    def call(self, msg):
+        pg.press('win')
+        pg.sleep(1)
+        pg.write('WhatsApp')
+        pg.sleep(2)
+        r = None
+        while r is None:
+            r = pg.locateCenterOnScreen('E:\\Projects\\ongoing_projects\\reminder\\images\\watsapp.png',
+                                        grayscale=True, confidence=.5)
+
+        pg.moveTo(r[0], r[1])
+        pg.click()
+
+        while True:
+            opened = pg.locateCenterOnScreen('E:\\Projects\\ongoing_projects\\reminder\\images\\chat.png',
+                                        grayscale=True, confidence=.5)
+            if opened:
+                break
+
+        # opening persons tab
+        pg.hotkey('ctrl', 'f')
+        pg.write(self.number)
+        pg.sleep(2)
+        pg.press('down')
+        pg.sleep(2)
+        pg.press('enter')
+        pg.sleep(1)
+
+        # send msg
+        pg.write(msg)
+        pg.sleep(1)
+        pg.press('enter')
+        pg.sleep(1)
+
+        # call
+        pg.click(1485, 101)
+        pg.sleep(5)
+
+        # cancel call
+        pg.click(895, 705)
+        pg.sleep(3)
+        pg.click(800, 450)
+        pg.hotkey('alt', 'f4')
+
+
+class Schedular:
+    def __init__(self, person: Person, *args):
+        self.person = person
+        self.jobs = args
+
+    def schedule_reminder(self):
+        for job in self.jobs:
+            schedule.every().day.at(job.time).do(self.person.call, job.reminder)
